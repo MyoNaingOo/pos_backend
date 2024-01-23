04e65fe96b439a2e596460dfe6052b1bff5d8deb
MyoNaingOo <myonaingoo623@gmail.com>
2024-01-23 22:41:44 +0630

    shop store api add

--- src/main/java/com/mno/business/Store/controller/StoreControl.java
+++ src/main/java/com/mno/business/Store/controller/StoreControl.java
@@ -2,18 +2,14 @@ package com.mno.business.Store.controller;
 
 
 import com.mno.business.Store.dto.StoreDto;
-import com.mno.business.Store.entity.Store;
 import com.mno.business.Store.service.StoreSer;
 import com.mno.business.config.JwtService;
 import com.mno.business.product.Repo.ProductRepo;
-import com.mno.business.product.entity.Product;
-import com.mno.business.user.entity.User;
 import com.mno.business.user.entity.UserInfo;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -29,7 +25,56 @@ public class StoreControl {
     @PostMapping("add")
     private void addStore(@RequestBody StoreDto storeDto, HttpServletRequest request) {
         UserInfo userInfo = jwtService.getUserInfo(request);
-        storeSer.addStore(userInfo,storeDto);
+        storeSer.addStore(userInfo, storeDto);
+    }
+
+
+
+    /*
+     *  shop api start
+     *
+     * */
+
+
+
+    @GetMapping("shop/page/{num}")
+    private List<StoreDto> pageOfShop(@PathVariable("num") int id,HttpServletRequest request) {
+        UserInfo userInfo = jwtService.getUserInfo(request);
+        return storeSer.resStoreDtos(storeSer.storeListOfShop(id,userInfo.getShop()));
+    }
+
+    @GetMapping("shop/prosBalance/{num}")
+    public List<StoreDto> getProductsBalanceOfShop(@PathVariable("num") int num,HttpServletRequest request) {
+        UserInfo userInfo = jwtService.getUserInfo(request);
+        return storeSer.resStoreDtos(storeSer.getProductsBalanceOfShop(num,userInfo.getShop()));
+    }
+
+    @GetMapping("shop/sold/{num}")
+    public List<StoreDto> getProductsSoldOfShop(@PathVariable("num") int num,HttpServletRequest request) {
+
+        UserInfo userInfo = jwtService.getUserInfo(request);
+        return storeSer.resStoreDtos(storeSer.getProductsSoldOfShop(num,userInfo.getShop()));
+    }
+
+
+    @GetMapping("shop/findAllByMonth/{num}")
+    private List<StoreDto> getByMonthOfShop(
+            @RequestParam("month") int month,
+            @RequestParam("year") int year,
+            @PathVariable("num") int num,HttpServletRequest request) {
+
+        UserInfo userInfo = jwtService.getUserInfo(request);
+        return storeSer.resStoreDtos(storeSer.findByMonthAndYearOfShop(month, year, num,userInfo.getShop()));
+    }
+
+
+    @GetMapping("shop/findAllByProduct/{num}")
+    private List<StoreDto> getAllByProductOfShop(
+            @PathVariable("num") int num,
+            @RequestParam("product_id") Long product_id,
+            HttpServletRequest request) {
+        UserInfo userInfo = jwtService.getUserInfo(request);
+        return storeSer.resStoreDtos(storeSer.findAllByProductOfShop(product_id, num,userInfo.getShop()));
     }
 
 
@@ -37,34 +82,16 @@ public class StoreControl {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    /*
+     *  shop api end
+     *
+     * */
 
     /*
-    *  Store All not shop
-    *
-    *
-    * */
+     *  Store All not shop
+     *
+     *
+     * */
 
 
     @GetMapping("page/{num}")
